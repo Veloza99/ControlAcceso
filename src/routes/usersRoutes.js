1f6c42bcd194e411e0d44df9a129b86a3ed18687
diego veloza <diego.veloza@unipamplona.edu.co>
2024-07-21 09:37:12 -0500

    Atorización de los Middlewares para los user

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,17 +2,18 @@ import { Router } from 'express';
 import { getAllUsers, getUserById, updateUser, deleteUser } from '../controllers/userController.js';
 import { validationResult } from 'express-validator';
 import {userValidation} from "./validadores.js";
+import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 // Ruta para conseguir todos los usuarios
-router.get('/', getAllUsers);
+router.get('/', authenticateToken, getAllUsers);
 
 // Ruta para buscar los usuarios por Id
-router.get('/:id', getUserById);
+router.get('/:id', authenticateToken, getUserById);
 
 // Ruta para actualizar los datos del usuario por id
-router.put('/:id', userValidation, async (req, res) => {
+router.put('/:id', authenticateToken, userValidation, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -21,6 +22,6 @@ router.put('/:id', userValidation, async (req, res) => {
 });
 
 // Eliminar usuario por id
-router.delete('/:id', deleteUser);
+router.delete('/:id', authenticateToken, deleteUser);
 
 export {router as usersRoutes}
