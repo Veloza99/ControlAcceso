846f71851d09b244431e40797187e348010a87f5
diego veloza <diego.veloza@unipamplona.edu.co>
2024-08-03 22:49:33 -0500

    Se modifico el validador de ruta de usuarios ya que tienen que ser opcional

--- src/validator/validadores.js
+++ src/validator/validadores.js
@@ -18,10 +18,11 @@ export const loginValidation = [
 ];
 
 // Validaciones para las rutas de usuario
+// tiene que ser opcional
 export const userValidation = [
-    check('firstName').notEmpty().withMessage('El nombre es requerido'),
-    check('lastName').notEmpty().withMessage('El apellido es requerido'),
-    check('identificacion').notEmpty().withMessage('La identificación es requerida'),
-    check('email').isEmail().withMessage('Por favor ingresa un correo electrónico válido'),
-    check('role').isIn([ESTUDIANTE, DOCENTE, ADMINISTRATIVO, VIGILANTE, VISITANTE]).withMessage('Rol inválido'),
+    check('firstName').optional().notEmpty().withMessage('El nombre no puede estar vacío si se proporciona'),
+    check('lastName').optional().notEmpty().withMessage('El apellido no puede estar vacío si se proporciona'),
+    check('identificacion').optional().notEmpty().withMessage('La identificación no puede estar vacía si se proporciona'),
+    check('email').optional().isEmail().withMessage('Por favor ingresa un correo electrónico válido'),
+    check('role').optional().isIn([ESTUDIANTE, DOCENTE, ADMINISTRATIVO, VIGILANTE, VISITANTE]).withMessage('Rol inválido'),
 ];
