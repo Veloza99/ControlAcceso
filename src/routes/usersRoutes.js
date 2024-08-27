b3d4893f17675d455627dd98bd50b5ffac7d300b
diego veloza <diego.veloza@unipamplona.edu.co>
2024-08-26 19:24:19 -0500

    se agrega endpoint para ctualizar imagen

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAllUsers, getUserById, updateUser, deleteUser } from '../controllers/userController.js';
+import { getAllUsers, getUserById, updateUser, updateUserProfilePic, deleteUser } from '../controllers/userController.js';
 import { validationResult } from 'express-validator';
 import { authenticateToken } from '../middlewares/authMiddleware.js';
 import {ADMIN } from "../config/constantes.js";
@@ -24,6 +24,17 @@ router.put('/:id', authenticateToken, authorize([ADMIN]), uploadImages.single('p
     }
     await updateUser(req, res);
 });
+
+// Ruta para actualizar solo la imagen de perfil del usuario
+router.put('/:id/picProfile', authenticateToken, authorize([ADMIN]), uploadImages.single('picProfile'), async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    await updateUserProfilePic(req, res);
+});
+
+
 // Eliminar usuario por id
 router.delete('/:id', authenticateToken, authorize([ADMIN]), deleteUser);
 
